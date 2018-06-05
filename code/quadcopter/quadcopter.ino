cac97851060d4cc2e02047c1be836a27b5604bc0
Jingbin Huang <ben441318936@users.noreply.github.com>
2018-06-05 13:43:35 -0700

    Defined ports in INO file

--- code/quadcopter/quadcopter.ino
+++ code/quadcopter/quadcopter.ino
@@ -1,12 +1,39 @@
-#define LED3 2
+#define PWM1 3
+#define PWM2 5
+#define PWM3 6
+#define PWM4 9
 
+#define debug1 2
+#define debug2 4
+
+#define SCL A5
+#define SDA A4
+
+#define CE 7
+#define CSN 8
+#define MOSI 11
+#define MISO 12
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(4, OUTPUT);
+  pinMode(PWM1,OUTPUT);
+  pinMode(PWM2,OUTPUT);
+  pinMode(PWM3,OUTPUT);
+  pinMode(PWM4,OUTPUT);
+  pinMode(debug1,OUTPUT);
+  pinMode(debug2,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(4, HIGH);
+  digitalWrite(debug1,HIGH);
+  digitalWrite(debug2,HIGH);
+
+  for(int i = 0; i < 100; i++){
+    analogWrite(PWM1,i);
+    delay(10);
+  }
+
+  digitalWrite(debug1,LOW);
+  digitalWrite(debug2,LOW);
+
+  delay(100);
 }
